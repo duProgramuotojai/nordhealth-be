88a5de3c5a8d451d8e033e4d2c09f6a9c293429c
hrkl <aj@patientsky.com>
2022-02-09 21:43:44 +0200

    added product ordering support

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 from django.shortcuts import render
 # import viewsets
 from rest_framework import viewsets, filters, generics, status, response
@@ -25,8 +27,9 @@ class ProductsViewSet(viewsets.ModelViewSet):
     # specify serializer to be used
     serializer_class = ProductsSerializer
 
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'barcode']
+    ordering_fields = ['title']
 
     def perform_create(self, serializer):
         serializer.save()
@@ -39,7 +42,18 @@ class ProductsViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_queryset(self):
-        return ProductsModel.objects
+        sorting_property = self.request.query_params.get('sort')
+        order = self.request.query_params.get('order')
+        products = ProductsModel.objects
+        properties = map(attrgetter('attname'), ProductsModel._meta.get_fields())
+        if sorting_property in properties:
+            products = products.order_by(sorting_property)
+            if order == "desc":
+                products = products.reverse()
+        elif sorting_property is not None:
+            print("sorting by invalid value: " + sorting_property)
+
+        return products
 
     def destroy(self, *args, **kwargs):
         super().destroy(*args, **kwargs)
